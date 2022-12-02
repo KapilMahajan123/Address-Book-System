ba69225b38c09e494ff1d2408bd0a1c2ceabb4bd
kapilmahajan123 <montyempire123gmail.com>
2022-12-02 12:28:21 +0530

    Added: Adding New Contacts

--- src/com/AddressBook/com/AddressBookMain.java
+++ src/com/AddressBook/com/AddressBookMain.java
@@ -1,5 +1,7 @@
 package com.AddressBook.com;
 
+import java.util.Scanner;
+
 /**
  * 
  * @author Kapil
@@ -8,9 +10,36 @@ package com.AddressBook.com;
 public class AddressBookMain {
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome To Address Book system Program ");
-		ContactPerson contactperson = new ContactPerson();  //Create object
-		System.out.println(contactperson.toString());     //compiler writes here contactperson.toString()
+		System.out.println("Welcome To Address Book");  //print welcome massage
+		AddressBook addressBook = new AddressBook();
+		Scanner scanner = new Scanner(System.in);    //Create object
+		//initalize
+		int Number;
+		
+        /**
+         * Enter number 1 to add this contacts details
+         * Enter number 2 to displaying contact details
+         */
+		do {
+			System.out.println(" 1. Add Contact \n 2. Display contact \n");
+			System.out.println("~Enter the  Number~: ");
+
+			Number = scanner.nextInt();    //
+
+			switch (Number) {
+
+			case 1:
+				addressBook.addContact();
+				break;
+			case 2:
+				addressBook.showContacts();
+				break;
+			default:
+				System.out.println(" !!!Invalid Input!!! ");
+				break;
+
+			}
+		} while (Number != 3);   //calling
 	}
 
 }
